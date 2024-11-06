f241cc6bb599cf8cdf627dc44467a39355b7571a
Mudalib Mohamed <mudalib310@gmail.com>
2024-11-06 16:14:59 +0300

    image was uploaded to uploads folder

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -4,12 +4,12 @@ const path = require("path");
 const filepath = "./data/tasks.json";
 
 exports.writeTasksToFile = (tasks) => {
-  fs.writeFileSync(filepath, JSON.stringify(tasks));
+  fs.writeFileSync(filepath, JSON.stringify(tasks, null, 2));
 };
 
 exports.readTasksFromFile = () => {
   if (!fs.existsSync(filepath)) {
-    this.writeTasksToFilesToFile([]);
+    this.writeTasksToFile([]);
   }
 
   const data = fs.readFileSync(filepath);
