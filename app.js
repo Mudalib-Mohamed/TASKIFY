6c592ece63f98f1a7bff5a0c13bbe26e53f7e1da
Mudalib Mohamed <mudalib310@gmail.com>
2024-11-06 10:15:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const taskRoutes = require("./routes/taskRoutes");
+
+const HOSTNAME = "localhost";
+const PORT = 7000;
+
+const server = http.createServer((req, res) => {
+  if (req.url.startsWith("/tasks")) {
+    taskRoutes(req, res);
+  } else {
+    res.writeHead(404, "Not Found", { "contant-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Sorry, Page not found!",
+      })
+    );
+  }
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`listening on port ${PORT}`);
+});
