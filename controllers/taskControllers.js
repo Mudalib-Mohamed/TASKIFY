0fe30d0c9427ab3820318890527e295df890cf17
Mudalib Mohamed <mudalib310@gmail.com>
2024-11-06 14:13:11 +0300

    createTask function was added to the taskControllers.js file

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -8,3 +8,42 @@ exports.getTasks = (req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify(tasks));
 };
+
+exports.createTask = (req, res) => {
+  const form = new IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Error parsing form.",
+        })
+      );
+      return;
+    }
+
+    const image = files.image[0];
+
+    const tasks = readTasksFromFile();
+
+    const newTask = {
+      id: Date.now(),
+      title: fields.title,
+      description: fields?.description || "",
+      status: fields?.status || "pending",
+      image: image ? `/uploads/${image.originalFilename}` : null,
+    };
+
+    tasks.push(newTask);
+
+    writeTasksToFile(tasks);
+
+    if (files.image) {
+      copyFileSync(
+        image.filepath,
+        path.join(__dirname, "../uploads", image.originalFilename)
+      );
+      res.end(JSON.stringify(newTask));
+    }
+  });
+};
